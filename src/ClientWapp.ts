3b03702a3d427ad6a7892dd7612378d686a68a2c
Felipe <felipe@txfuel.com.br>
2024-06-11 12:01:56 -0300

    SOU FODA

--- src/ClientWapp.ts
+++ src/ClientWapp.ts
@@ -1,61 +1,75 @@
-import qrcode from 'qrcode-terminal';
-import { Client, LocalAuth } from 'whatsapp-web.js';
-import { TextMessage } from './TextMessage';
-import { ImageMessage } from './ImgMessage';
+// ClientWapp.ts
+import qrcode from "qrcode-terminal";
+import { Client, LocalAuth } from "whatsapp-web.js";
+import { TextMessage } from "./TextMessage";
+import { ImageMessage } from "./ImgMessage";
 
 export default class ClientWapp {
-    private c: Client;
-    private txt: TextMessage;
-    private img: ImageMessage;
+  private c: Client;
+  private txt: TextMessage;
+  private img: ImageMessage;
+  private status: String;
 
-    constructor() {
-        this.c = new Client({
-            authStrategy: new LocalAuth(),
-            webVersionCache: {
-                type: 'remote',
-                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-            },
-        });
-  
-        this.initialize();
+  constructor() {
+    this.c = new Client({
+      authStrategy: new LocalAuth(),
+      webVersionCache: {
+        type: "remote",
+        remotePath:
+          "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+      },
+    });
 
-        this.txt = new TextMessage(this.c)
-        this.img = new ImageMessage(this.c)
+    this.status = "disconnected";
+
+    this.initialize();
+
+    this.txt = new TextMessage(this.c);
+    this.img = new ImageMessage(this.c);
+  }
+
+  private initialize() {
+    this.c.on("qr", (qr) => {
+      qrcode.generate(qr, { small: true });
+        this.status = "qr";
+    });
+
+    this.c.on("authenticated", () => {
+      console.log("Autenticado com sucesso!");
+      this.status = "authenticated";
+    });
+
+    this.c.on("auth_failure", (msg) => {
+      console.error("Falha na autenticação:", msg);
+      this.status = "auth_failure";
+    });
+
+    this.c.on("ready", () => {
+      console.log("Client conectado!");
+        this.status = "ready";
+    });
+
+    this.c.on("disconnected", (reason) => {
+      console.log("Cliente desconectado:", reason);
+        this.status = "disconnected";
+    });
+
+    this.c.initialize();
+  }
+
+  public get getClient() {
+    return this.c;
+  }
+
+    public get getStatus() {
+        return this.status;
     }
 
-    private initialize() {
-        this.c.on('qr', (qr) => {
-            qrcode.generate(qr, { small: true });
-        });
+  public get getImg() {
+    return this.img;
+  }
 
-        this.c.on('authenticated', () => {
-            console.log('Autenticado com sucesso!');
-        });
-
-        this.c.on('auth_failure', msg => {
-            console.error('Falha na autenticação:', msg);
-        });
-
-        this.c.on('ready', () => {
-            console.log('Client conectado!');
-        });
-
-        this.c.on('disconnected', (reason) => {
-            console.log('Cliente desconectado:', reason);
-        });
-
-        this.c.initialize();
-    }
-
-    public get getClient() {
-        return this.c;
-    }
-
-    public get getImg() {
-        return this.img;
-    }
-
-    public get getTxt() {
-        return this.txt;
-    }
+  public get getTxt() {
+    return this.txt;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+// Main.ts
 import ClientWapp from './ClientWapp';
 
+class Main {
+    public static async main() {
+        const c = new ClientWapp();
 
-class Main{
-    public static async main(){
-        const c = new ClientWapp;
+        // Aguarda a inicialização do cliente
+        // await new Promise<void>((resolve) => {
+        //     c.getClient.on('ready', () => {
+        //         resolve();
+        //     });
+        // });
 
-        c.getTxt.textMessage('554197825044@c.us', 'funcionou essa bomba, manda uma img pra teste')
+        console.log('Status:', c.getStatus);
 
-        c.getImg.downloadImage
+        if(c.getStatus == 'ready') {
+            await c.getTxt.textMessage('554197825044@c.us', 'funcionou essa bomba, manda uma img pra teste');
+        }
+
+        // await c.getImg.downloadImage(); // Supondo que `downloadImage` seja um método assíncrono
     }
 }
- 
+
+// Chama o método main
+Main.main();
